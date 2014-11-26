07753be0c58509f791e6f81a9e907dc98d3e53a1
Englund92 <S_englund0110@hotmail.com>
2014-11-26 16:20:57 +0100

    a test view for tommorow is up

---
+++ public/app/view5/view5.js
@@ -0,0 +1,12 @@
+'use strict';
+
+angular.module('myAppRename.view5', ['ngRoute'])
+
+    .config(['$routeProvider', function($routeProvider) {
+        $routeProvider.when('/view5', {
+            templateUrl: 'app/view5/view5.html'
+        });
+    }])
+
+    .controller('View5Ctrl', function() {
+    });
