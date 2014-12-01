568bdef88a9120e3a2d9da81689e5d29d097b5bd
Englund92 <S_englund0110@hotmail.com>
2014-12-01 12:42:19 +0100

    Many changes

--- public/app/view5/view5.js
+++ public/app/view5/view5.js
@@ -10,24 +10,26 @@ angular.module('myAppRename.view5', ['ngRoute'])
     }])
 
     .controller('View5Ctrl', function ($scope, $http) {
+        $scope.getMovie = function(title, year, showIt){
+            var showBut
+            if(showIt==="undefined"){
+                $scope.showBut = false
+            } else {
+                $scope.showBut = showIt
+            }
 
-        $scope.getMovie = function(title, year){
-            console.log(title, year);
             $http({
                 method: 'GET',
                 url: 'http://www.omdbapi.com/?t=' + title +'&y=' + year + '&plot=long&r=json',
                 dataType: 'json'
             }).
                 success(function (data, status, headers, config) {
-
                     $scope.posts = data;
                 }).
                 error(function (data, status, headers, config) {
                     $scope.error = data;
                 });}
-
             $scope.saveMovie = function(Id, Rating, Year, Title, Genre, Runtime){
-
                 console.log(Id, Title, Genre, Year, Runtime, Rating);
                 var urlStr = '/test/addtitle/test/' + Id + '/' + Rating + '/' + Year + '/' + Title + '/' + Genre + '/' + Runtime +''
                 console.log(urlStr);
@@ -35,18 +37,12 @@ angular.module('myAppRename.view5', ['ngRoute'])
                 $http({
                     method: 'POST',
                     url: urlStr
-
-
                 }).
                     success(function (data, status) {
                         $scope.status = status;
-
-
                     }).
                     error(function (data, status, error) {
                         $scope.error = status;
-
                     });
         }
-
     });
